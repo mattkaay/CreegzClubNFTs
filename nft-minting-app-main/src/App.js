8f61bafdd84e00b438508d1cb4cb2f95628a9c66
Matthew <104280624+mattkaay@users.noreply.github.com>
2022-05-15 10:23:47 +1200

    Swap mintForAddress params

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -130,7 +130,7 @@ function App() {
     setFeedback(`Minting your ${CONFIG.NFT_NAME}...`);
     setClaimingNft(true);
     blockchain.smartContract.methods
-      .mintForAddress(blockchain.account, mintAmount)
+      .mintForAddress(mintAmount, blockchain.account)
       .send({
         gasLimit: String(totalGasLimit),
         to: CONFIG.CONTRACT_ADDRESS,
